1c7f25d59ec7b04be4ed268724ac5b3c1e4ccba8
Yatkins <tzippi8@gmail.com>
2020-12-21 17:57:56 -0600

    fixed

--- src/main/java/com/example/RestTemplateA/RestTemplateAApplication.java
+++ src/main/java/com/example/RestTemplateA/RestTemplateAApplication.java
@@ -3,11 +3,15 @@ package com.example.RestTemplateA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RestTemplateAApplication {
 
 	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate(); }
+
 	public static void main(String[] args) {
 		SpringApplication.run(RestTemplateAApplication.class, args);
 	}
